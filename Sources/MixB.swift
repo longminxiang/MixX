c28e1abcff0b20dbd4848357727f451560eafd05
Eric <longminxiang@163.com>
2022-10-27 18:48:19 +0800

    add onChange param

--- Sources/MixB.swift
+++ Sources/MixB.swift
@@ -17,15 +17,20 @@ import SwiftUI
 ///     var body: some View {
 ///         MixB($name) { binding in
 ///             TextField("input name", text: binding)
+///         } onChange: { name in
+///             debugPrint("change \(name)")
 ///         }
 ///     }
 /// }
 /// ```
 ///
 public struct MixB<Content: View, Value: Equatable>: View {
+    
+    public typealias OnChange<Value: Equatable> = (Value) -> Void
 
     private class Holder<Value: Equatable>: ObservableObject {
         let binding: MixO<Value>
+        let onChange: OnChange<Value>?
         @Published var bindingValue: Value {
             didSet {
                 guard binding.wrappedValue != bindingValue else { return }
@@ -33,14 +38,16 @@ public struct MixB<Content: View, Value: Equatable>: View {
             }
         }
 
-        init(_ binding: MixO<Value>) {
+        init(_ binding: MixO<Value>, onChange: OnChange<Value>?) {
             self.binding = binding
+            self.onChange = onChange
             self.bindingValue = binding.wrappedValue
 
             MixXCenter.add(self, keys: [binding.key]) { [weak self] info in
                 guard let s = self else { return }
                 guard s.bindingValue != s.binding.wrappedValue else { return }
                 s.bindingValue = binding.wrappedValue
+                s.onChange?(s.bindingValue)
             }
         }
     }
@@ -49,8 +56,8 @@ public struct MixB<Content: View, Value: Equatable>: View {
     private var builder: (Binding<Value>) -> Content
 
     /// Initor
-    public init(_ obj: MixO<Value>, @ViewBuilder builder: @escaping (Binding<Value>) -> Content) {
-        self.holder = Holder(obj)
+    public init(_ obj: MixO<Value>, @ViewBuilder builder: @escaping (Binding<Value>) -> Content, onChange: OnChange<Value>? = nil) {
+        self.holder = Holder(obj, onChange: onChange)
         self.builder = builder
     }
 
--- Sources/MixX.swift
+++ Sources/MixX.swift
@@ -43,13 +43,18 @@ import SwiftUI
 /// ```
 ///
 public struct MixX<Content: View>: View {
+    
+    public typealias OnChange = () -> Void
 
     private class Trigger: ObservableObject {
         @Published private(set) var i: Bool = false
-
-        init(_ keys: [MixXCenter.Key]) {
+        private let onChange: OnChange?
+        
+        init(_ keys: [MixXCenter.Key], onChange: OnChange?) {
+            self.onChange = onChange
             MixXCenter.add(self, keys: keys) { [weak self] info in
                 self?.i.toggle()
+                self?.onChange?()
             }
         }
     }
@@ -60,9 +65,9 @@ public struct MixX<Content: View>: View {
     private var builder: Builder
 
     /// Initor
-    public init(_ keys: [MixXCenter.Key], @ViewBuilder builder: @escaping Builder) {
+    public init(_ keys: [MixXCenter.Key], @ViewBuilder builder: @escaping Builder, onChange: OnChange? = nil) {
         self.builder = builder
-        self.trigger = Trigger(keys)
+        self.trigger = Trigger(keys, onChange: onChange)
     }
 
     /// body
@@ -80,47 +85,78 @@ extension MixX {
     public typealias Builder5<T1, T2, T3, T4, T5> = (_ vals: (T1, T2, T3, T4, T5)) -> Content
     public typealias Builder6<T1, T2, T3, T4, T5, T6> = (_ vals: (T1, T2, T3, T4, T5, T6)) -> Content
 
+    public typealias OnChange1<T1> = (T1) -> Void
+    public typealias OnChange2<T1, T2> = (_ vals: (T1, T2)) -> Void
+    public typealias OnChange3<T1, T2, T3> = (_ vals: (T1, T2, T3)) -> Void
+    public typealias OnChange4<T1, T2, T3, T4> = (_ vals: (T1, T2, T3, T4)) -> Void
+    public typealias OnChange5<T1, T2, T3, T4, T5> = (_ vals: (T1, T2, T3, T4, T5)) -> Void
+    public typealias OnChange6<T1, T2, T3, T4, T5, T6> = (_ vals: (T1, T2, T3, T4, T5, T6)) -> Void
+
     /// Initor
-    public init<T1>(_ obj: MixO<T1>, @ViewBuilder builder: @escaping Builder1<T1>) {
+    public init<T1>(
+        _ obj: MixO<T1>,
+        @ViewBuilder builder: @escaping Builder1<T1>,
+        onChange: OnChange1<T1>? = nil)
+    {
         self.init([obj.key]) {
             builder(obj.wrappedValue)
+        } onChange: {
+            onChange?(obj.wrappedValue)
         }
     }
 
     /// Initor
-    public init<T1, T2>(_ o1: MixO<T1>, _ o2: MixO<T2>, @ViewBuilder builder: @escaping Builder2<T1, T2>) {
+    public init<T1, T2>(
+        _ o1: MixO<T1>, _ o2: MixO<T2>,
+        @ViewBuilder builder: @escaping Builder2<T1, T2>,
+        onChange: OnChange2<T1, T2>? = nil)
+    {
         self.init([o1.key, o2.key]) {
             builder((o1.wrappedValue, o2.wrappedValue))
+        } onChange: {
+            onChange?((o1.wrappedValue, o2.wrappedValue))
         }
     }
 
     /// Initor
     public init<T1, T2, T3>(
         _ o1: MixO<T1>, _ o2: MixO<T2>, _ o3: MixO<T3>,
-        @ViewBuilder builder: @escaping Builder3<T1, T2, T3>)
+        @ViewBuilder builder: @escaping Builder3<T1, T2, T3>,
+        onChange: OnChange3<T1, T2, T3>? = nil)
     {
         self.init([o1.key, o2.key, o3.key]) {
             builder((o1.wrappedValue, o2.wrappedValue, o3.wrappedValue))
+        } onChange: {
+            onChange?((o1.wrappedValue, o2.wrappedValue, o3.wrappedValue))
         }
     }
 
     /// Initor
     public init<T1, T2, T3, T4>(
         _ o1: MixO<T1>, _ o2: MixO<T2>, _ o3: MixO<T3>, _ o4: MixO<T4>,
-        @ViewBuilder builder: @escaping Builder4<T1, T2, T3, T4>)
+        @ViewBuilder builder: @escaping Builder4<T1, T2, T3, T4>,
+        onChange: OnChange4<T1, T2, T3, T4>? = nil)
     {
         self.init([o1.key, o2.key, o3.key, o4.key]) {
             builder((o1.wrappedValue, o2.wrappedValue, o3.wrappedValue, o4.wrappedValue))
+        } onChange: {
+            onChange?((o1.wrappedValue, o2.wrappedValue, o3.wrappedValue, o4.wrappedValue))
         }
     }
 
     /// Initor
     public init<T1, T2, T3, T4, T5>(
         _ o1: MixO<T1>, _ o2: MixO<T2>, _ o3: MixO<T3>, _ o4: MixO<T4>, _ o5: MixO<T5>,
-        @ViewBuilder builder: @escaping Builder5<T1, T2, T3, T4, T5>)
+        @ViewBuilder builder: @escaping Builder5<T1, T2, T3, T4, T5>,
+        onChange: OnChange5<T1, T2, T3, T4, T5>? = nil)
     {
         self.init([o1.key, o2.key, o3.key, o4.key, o5.key]) {
             builder((
+                o1.wrappedValue, o2.wrappedValue, o3.wrappedValue,
+                o4.wrappedValue, o5.wrappedValue
+            ))
+        } onChange: {
+            onChange?((
                 o1.wrappedValue, o2.wrappedValue, o3.wrappedValue,
                 o4.wrappedValue, o5.wrappedValue
             ))
@@ -131,10 +167,16 @@ extension MixX {
     public init<T1, T2, T3, T4, T5, T6>(
         _ o1: MixO<T1>, _ o2: MixO<T2>, _ o3: MixO<T3>,
         _ o4: MixO<T4>, _ o5: MixO<T5>, _ o6: MixO<T6>,
-        @ViewBuilder builder: @escaping Builder6<T1, T2, T3, T4, T5, T6>)
+        @ViewBuilder builder: @escaping Builder6<T1, T2, T3, T4, T5, T6>,
+        onChange: OnChange6<T1, T2, T3, T4, T5, T6>? = nil)
     {
         self.init([o1.key, o2.key, o3.key, o4.key, o5.key, o6.key]) {
             builder((
+                o1.wrappedValue, o2.wrappedValue, o3.wrappedValue,
+                o4.wrappedValue, o5.wrappedValue, o6.wrappedValue
+            ))
+        } onChange: {
+            onChange?((
                 o1.wrappedValue, o2.wrappedValue, o3.wrappedValue,
                 o4.wrappedValue, o5.wrappedValue, o6.wrappedValue
             ))
